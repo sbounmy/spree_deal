6e48a46f2e3856c0cf0193e21ed482b0c5d309af
Bounmy Stephane <stephanebounmy@gmail.com>
2012-02-12 22:05:08 +0100

    admin deal

--- app/models/spree/deal.rb
+++ app/models/spree/deal.rb
@@ -1,2 +1,4 @@
 class Spree::Deal < ActiveRecord::Base
+  validates_presence_of :name, :product_id, :starts_at, :expires_at
+  belongs_to :product
 end
--- spec/models/spree/deal_spec.rb
+++ spec/models/spree/deal_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Spree::Deal do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :product_id }
+  it { should validate_presence_of :starts_at }
+  it { should validate_presence_of :expires_at }
 end
