a248cad8dde5b723a5e5b70e395cbd1f52258499
Bounmy Stephane <stephanebounmy@gmail.com>
2012-02-17 16:34:39 +0100

    displaying deal info

--- app/overrides/cart_form.rb
+++ app/overrides/cart_form.rb
@@ -1,3 +1,3 @@
-Deface::Override.new :name => "cart_form", :replace_contents => "#product-price div", :partial => "spree/deals/cart_form", :virtual_path => "spree/products/_cart_form"
-Deface::Override.new :name => "products_list_item_price", :replace => "[itemprop='price']", :partial => "spree/deals/cart_form", :virtual_path => "spree/shared/_products"
+Deface::Override.new :name => "cart_form", :insert_bottom => "[data-hook='product_price']", :partial => "spree/deals/cart_form", :virtual_path => "spree/products/_cart_form"
+Deface::Override.new :name => "products_list_item_price", :replace => "[itemprop='price']", :partial => "spree/deals/product_price", :virtual_path => "spree/shared/_products"
 
