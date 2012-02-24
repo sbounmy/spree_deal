5e5c14a86c61aa5823f418d08a508bf56237f311
Bounmy Stephane <stephanebounmy@gmail.com>
2012-02-24 16:21:53 +0100

    removed include spree url helpers

--- app/overrides/add_deals_tab.rb
+++ app/overrides/add_deals_tab.rb
@@ -1,4 +1,3 @@
-include Spree::UrlHelpers
 include Spree::Admin::NavigationHelper
 
 Deface::Override.new :name => "add_deals_tab", :insert_bottom => "ul[data-hook='admin_tabs']", :text => %q{<%= tab(:deals) %>}, :virtual_path => "spree/layouts/admin"
