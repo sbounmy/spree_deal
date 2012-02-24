6613fd470100cdb0994f1c0003b669daf3004825
Bounmy Stephane <stephanebounmy@gmail.com>
2012-02-24 12:34:29 +0100

    fix undefined route

--- app/overrides/order_inventory_status.rb
+++ app/overrides/order_inventory_status.rb
@@ -1,5 +1,3 @@
-Deface::Override.new :name => "cart_form", :replace => "[data-hook='account_my_orders'] tbody code[erb-loud]:contains('t(order.state).titleize')", :text => %q{<%= link_to(t(order.state).titleize, order_inventory_units_path(order)) -%>}, :virtual_path => "spree/users/show"
-
 Deface::Override.new :name => "order_details_header", :insert_before => "#line-items tr[data-hook='order_details_line_items_headers'] th.price", :text => "<th class='price'><%= t(:state) %></th>", :virtual_path => "spree/shared/_order_details"
 
 Deface::Override.new :name => "order_details", :insert_after => "[data-hook='order_item_description']", :partial => "spree/deals/order_details", :virtual_path => "spree/shared/_order_details"
